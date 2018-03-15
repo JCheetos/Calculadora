71a4924f49127078f5970d52fb40aff8782e4215
JCheetos <36264544+JCheetos@users.noreply.github.com>
2018-03-15 03:47:40 -0500

    Conversiones Listas
    Ya convierte entre cualquier numero que se encuentre en pantalla, ahora falta que opere >:V

--- Calculadora/src/calculadora/logica/Calculadora.java
+++ Calculadora/src/calculadora/logica/Calculadora.java
@@ -3,7 +3,8 @@
  * Copyright (c) 2018 Valentina Tobo 20172020063, Camilo Torres 20172020067, Johan Mendez 20172020070
  */
 package calculadora.logica;
-import  java.lang.Integer;
+
+import java.lang.Integer;
 
 /**
  * La clase Calculadora define y maneja las variables necesarias para el
@@ -17,98 +18,11 @@ public class Calculadora {
 
     private String numeroVisualizado;
     private int numeroGuardado;
-    private BasesNumericas binario;
-    private BasesNumericas decimal;
-    private BasesNumericas octal;
-    private BasesNumericas hexadecimal;
 
     public Calculadora() {
         numeroVisualizado = "0";
         numeroGuardado = 0;
-        binario = new BasesNumericas(2);
-        decimal = new BasesNumericas(10);
-        octal = new BasesNumericas(8);
-        hexadecimal = new BasesNumericas(16);
-    }
 
-    /**
-     * La funcion se encarga de retornar el valor de binario del objeto tipo
-     * Calculadora del cual es llamada
-     *
-     * @return binario
-     */
-    public BasesNumericas getBinario() {
-        return binario;
-    }
-
-    /**
-     * La funcion se encarga de definir el valor del binario del objeto tipo
-     * Calculadora del cual es llamado
-     *
-     * @param binario
-     */
-    public void setBinario(BasesNumericas binario) {
-        this.binario = binario;
-    }
-
-    /**
-     * La funcion se encarga de retornar el valor del decimal del objeto tipo
-     * Calculadora del cual es llamada
-     *
-     * @return decimal
-     */
-    public BasesNumericas getDecimal() {
-        return decimal;
-    }
-
-    /**
-     * La funcion se encarga de definir el valor del decimal del objeto tipo
-     * Calculadora del cual es llamado
-     *
-     * @param decimal
-     */
-    public void setDecimal(BasesNumericas decimal) {
-        this.decimal = decimal;
-    }
-
-    /**
-     * La funcion se encarga de retornar el valor del octal del objeto tipo
-     * Calculadora del cual es llamada
-     *
-     * @return octal
-     */
-    public BasesNumericas getOctal() {
-        return octal;
-    }
-
-    /**
-     * La funcion se encarga de definir el valor del octal del objeto tipo
-     * Calculadora del cual es llamado
-     *
-     * @param octal
-     */
-    public void setOctal(BasesNumericas octal) {
-        this.octal = octal;
-    }
-
-    /**
-     * La funcion se encarga de retornar el valor del hexadecimal del objeto
-     * tipo Calculadora del cual es llamada
-     *
-     * @return hexagesimal
-     */
-    public BasesNumericas getHexadecimal() {
-        return hexadecimal;
-    }
-
-    /**
-     * La funcion se encarga de definir el valor del hexadecimal del objeto tipo
-     * Calculadora del cual es llamado
-     *
-     * @param hexadecimal
-     */
-    public void setHexadecimal(BasesNumericas hexadecimal) {
-        this.hexadecimal = hexadecimal;
     }
 
     public String actualizarNumero(String numeroVisualizado, int basea, int basen) {
@@ -124,12 +38,11 @@ public class Calculadora {
                 numeroVisualizado = Integer.toOctalString(numeroGuardado);
                 break;
             default:
-               numeroVisualizado = Integer.toHexString(numeroGuardado);
+                numeroVisualizado = Integer.toHexString(numeroGuardado);
                 break;
         }
         return numeroVisualizado;
     }
-
 
     public String NumeroEnPantalla(String numeroVisualizado, int numero) {
 
@@ -161,18 +74,79 @@ public class Calculadora {
         int numerodecimal;
         int exponente;
         numerodecimal = 0;
-        char[] arrayChar = numeroVisualizado.toCharArray();
+        char[] arrayChar;
+        arrayChar = numeroVisualizado.toCharArray();
         for (int i = 0; i < arrayChar.length; i++) {
             exponente = arrayChar.length - (i + 1);
             switch (base) {
                 case 1:
-                    numerodecimal = numerodecimal + (arrayChar[i] * (int) Math.pow(2, exponente));
+                    switch (arrayChar[i]) {
+                        case '1':
+                            numerodecimal = numerodecimal + (1 * (int) Math.pow(2, exponente));
+                            break;
+                        case '0':
+                            break;
+                    }
                     break;
                 case 2:
-                    numerodecimal = numerodecimal + (arrayChar[i] * (int) Math.pow(10, exponente));
+                    switch (arrayChar[i]) {
+                        case '1':
+                            numerodecimal = numerodecimal + (1 * (int) Math.pow(10, exponente));
+                            break;
+                        case '2':
+                            numerodecimal = numerodecimal + (2 * (int) Math.pow(10, exponente));
+                            break;
+                        case '3':
+                            numerodecimal = numerodecimal + (3 * (int) Math.pow(10, exponente));
+                            break;
+                        case '4':
+                            numerodecimal = numerodecimal + (4 * (int) Math.pow(10, exponente));
+                            break;
+                        case '5':
+                            numerodecimal = numerodecimal + (5 * (int) Math.pow(10, exponente));
+                            break;
+                        case '6':
+                            numerodecimal = numerodecimal + (6 * (int) Math.pow(10, exponente));
+                            break;
+                        case '7':
+                            numerodecimal = numerodecimal + (7 * (int) Math.pow(10, exponente));
+                            break;
+                        case '8':
+                            numerodecimal = numerodecimal + (8 * (int) Math.pow(10, exponente));
+                            break;
+                        case '9':
+                            numerodecimal = numerodecimal + (9 * (int) Math.pow(10, exponente));
+                            break;
+                        case '0':
+                            break;
+                    }
                     break;
                 case 3:
-                    numerodecimal = numerodecimal + (arrayChar[i] * (int) Math.pow(8, exponente));
+                    switch (arrayChar[i]) {
+                        case '1':
+                            numerodecimal = numerodecimal + (1 * (int) Math.pow(8, exponente));
+                            break;
+                        case '2':
+                            numerodecimal = numerodecimal + (2 * (int) Math.pow(8, exponente));
+                            break;
+                        case '3':
+                            numerodecimal = numerodecimal + (3 * (int) Math.pow(8, exponente));
+                            break;
+                        case '4':
+                            numerodecimal = numerodecimal + (4 * (int) Math.pow(8, exponente));
+                            break;
+                        case '5':
+                            numerodecimal = numerodecimal + (5 * (int) Math.pow(8, exponente));
+                            break;
+                        case '6':
+                            numerodecimal = numerodecimal + (6 * (int) Math.pow(8, exponente));
+                            break;
+                        case '7':
+                            numerodecimal = numerodecimal + (7 * (int) Math.pow(8, exponente));
+                            break;
+                        case '0':
+                            break;
+                    }
                     break;
                 case 4:
                     switch (arrayChar[i]) {
@@ -227,17 +201,17 @@ public class Calculadora {
                     break;
             }
         }
-        System.out.println(numerodecimal);
         return numerodecimal;
     }
-    public int obtenerValorFormateado(int numeroGuardado, int base) {
+
+    public int obtenerValorFormateado(int numeroGuardado, int basen) {
         int numerodecimal;
         int exponente;
         numerodecimal = 0;
         char[] arrayChar = numeroVisualizado.toCharArray();
         for (int i = 0; i < arrayChar.length; i++) {
             exponente = arrayChar.length - (i + 1);
-            switch (base) {
+            switch (basen) {
                 case 1:
                     numerodecimal = numerodecimal + (arrayChar[i] * (int) Math.pow(2, exponente));
                     break;
@@ -245,7 +219,31 @@ public class Calculadora {
                     numerodecimal = numerodecimal + (arrayChar[i] * (int) Math.pow(10, exponente));
                     break;
                 case 3:
-                    numerodecimal = numerodecimal + (arrayChar[i] * (int) Math.pow(8, exponente));
+                    switch (arrayChar[i]) {
+                        case '1':
+                            numerodecimal = numerodecimal + (1 * (int) Math.pow(8, exponente));
+                            break;
+                        case '2':
+                            numerodecimal = numerodecimal + (2 * (int) Math.pow(8, exponente));
+                            break;
+                        case '3':
+                            numerodecimal = numerodecimal + (3 * (int) Math.pow(8, exponente));
+                            break;
+                        case '4':
+                            numerodecimal = numerodecimal + (4 * (int) Math.pow(8, exponente));
+                            break;
+                        case '5':
+                            numerodecimal = numerodecimal + (5 * (int) Math.pow(8, exponente));
+                            break;
+                        case '6':
+                            numerodecimal = numerodecimal + (6 * (int) Math.pow(8, exponente));
+                            break;
+                        case '7':
+                            numerodecimal = numerodecimal + (7 * (int) Math.pow(8, exponente));
+                            break;
+                        case '0':
+                            break;
+                    }
                     break;
                 case 4:
                     switch (arrayChar[i]) {
